1dc74d5abe64ae9e6378daab21f55c3a0d9f7140
Jose Gomez-Dans <jgomezdans@gmail.com>
2012-05-21 12:42:30 +0100

    New setup file

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = '1.0'
+DISTNAME = "eoldas"
+DESCRIPTION = "An Earth Observation Land Data Assimilation System (EO-LDAS)"
+LONG_DESCRIPTION = open('README.rst').read()
+MAINTAINER = 'Jose Gomez-Dans/NCEO & University College London'
+MAINTAINER_EMAIL = "j.gomez-dans@ucl.ac.uk"
+URL = 'http://github.com/jgomezdans/eoldas'
+LICENSE = 'Undecided'
+VERSION = "1.0.1"
+DOWNLOAD_URL="https://github.com/jgomezdans/semidiscrete/zipball/master"
 
 setup(name='eoldas',
       version=version,
-      description="An Earth Observation Land Data Assimilation System (EO-LDAS)",
-      long_description="""\
-""",
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='J Gomez-Dans',
-      author_email='j.gomez-dans@ucl.ac.uk',
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
       url='http://www.assimila.eu/eoldas',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
@@ -19,8 +27,19 @@ setup(name='eoldas',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+          # It require the RT codes, but the package names are still in flux
+          ],
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved',
+              'Programming Language :: Fortran',
+              'Programming Language :: Python',
+              'Topic :: Software Development',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS'
+              ],
       )
